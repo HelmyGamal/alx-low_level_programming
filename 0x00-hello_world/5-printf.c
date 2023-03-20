138a72a61f8acdb1d84f553b320f5102ab326ee4
HelmyGamal <helmygamal79@gmail.com>
2023-03-20 12:57:04 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : C programe that prints using printf function
+ *
+ * Return: return always 0 (success)
+ *
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

e41f1ac6203c524bb87512fdfc16b4e22bc6af45
HelmyGamal <helmygamal79@gmail.com>
2023-03-22 15:24:56 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 -  Description : C programe that prints
+ *			lower case alpha ten times
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 1; i <= 10; i++)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+	}
+}

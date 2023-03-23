3a1c66a74fe4927f126db8631e76aa62c7300618
HelmyGamal <helmygamal79@gmail.com>
2023-03-22 19:50:32 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * add -  Description : C programe checks nums
+ *
+ * @a : input of the function
+ *
+ * @b : fun input
+ *
+ * Return: return the add result
+ *
+*/
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}

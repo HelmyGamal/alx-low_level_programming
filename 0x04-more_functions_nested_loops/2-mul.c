1f6fee590fa926924728370902613f9a741b8864
HelmyGamal <helmygamal79@gmail.com>
2023-03-23 07:52:18 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * mul -  Description : C programe a*b
+ *
+ * @a : input of the function
+ *
+ * @b : fun input
+ *
+ * Return: return the add result
+ *
+*/
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}

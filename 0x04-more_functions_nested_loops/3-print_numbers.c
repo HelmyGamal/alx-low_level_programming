d1b1375bc45e84fcd58cab570cb79513bf00b419
HelmyGamal <helmygamal79@gmail.com>
2023-03-23 08:01:56 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_numbers -  Description : C programe that prints nums
+*/
+void print_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}

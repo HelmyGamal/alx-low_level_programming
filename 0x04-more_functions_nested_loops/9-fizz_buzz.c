8e79861e82524f3a7587fda2889225d11f849a46
HelmyGamal <helmygamal79@gmail.com>
2023-03-23 18:55:17 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int main(void)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("fizzBazz");
+			printf("fizzBuzz");
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && !(i % 5 == 0))
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && !(i % 3 == 0))
 		{
-			printf("Bazz");
+			printf("Buzz");
 		}
 		else
 		{

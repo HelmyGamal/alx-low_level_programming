3e4dcd220d824e69e3a8829d5b865f8b7cf58f6a
HelmyGamal <helmygamal79@gmail.com>
2023-03-27 16:00:18 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string, followed by a new line.
+ *
+ * @str : the string
+ *
+ * Return nothing
+ *
+*/
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str += 2;
+	}
+	_putchar('\n');
+}

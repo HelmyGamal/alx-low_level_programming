03250babeb2ea397ba697821cd481d8801db00f4
HelmyGamal <helmygamal79@gmail.com>
2023-03-30 07:11:58 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - prints an uppercase string.
+ *
+ * @str : the string
+ *
+ * Return: uppercase string
+ *
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\n'; i++)
+	{
+		if (str[i] >= 97 && srt[i] <= 122)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}

613d443dba366fa30660f93182a88d735b1f487d
HelmyGamal <helmygamal79@gmail.com>
2023-04-03 08:06:59 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function that copy two strings..
+ *
+ * @dest : first string.
+ * @src :second string
+ * @n: n bytes from src
+ *
+ * Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n && src[k] != '\0'; k++)
+		dest[k] = src[k];
+	return (dest);
+}

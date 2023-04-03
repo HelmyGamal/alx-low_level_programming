78f3e69e19b9984c50da4d629ad94077ff8e2bab
HelmyGamal <helmygamal79@gmail.com>
2023-04-03 07:56:34 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_memset - function that copy two strings..
+ *
+ * @s : first string.
+ * @b :second string
+ * @n: n bytes from src
+ *
+ * Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+		s[k] = b;
+	/*while (k < n)
+	{
+		s[k] = '\0';
+		k++;
+	}*/
+	return (s);
+}
